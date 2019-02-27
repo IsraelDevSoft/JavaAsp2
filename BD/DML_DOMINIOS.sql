91492a635f253ce541b4aeac668f5001c529e736
israel1423 <santanaisrael62@gmail.com>
2019-02-26 20:12:33 -0500

    Actualizacion de la tabla dominio

--- BD/DML_DOMINIOS.sql
+++ BD/DML_DOMINIOS.sql
@@ -3,4 +3,5 @@ use videotienda;
 insert into dominios values
 ('TIPO_DOC' , 'CC' , 'Cedula de Ciudadania'),
 ('TIPO_DOC' , 'CE' , 'Cedula de Extranjeria'),
-('TIPO_DOC' , 'PA' , 'Pasaporte');
+('TIPO_DOC' , 'PA' , 'Pasaporte'),
+('TIPO_DOC' , 'TI' , 'Tarjeta de Identidad');
